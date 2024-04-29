fedbda9b41e0e21ada09ec95780e6513e8e5407f
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-04-29 22:34:12 +0500

    Add py and rb

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find ../$REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" \) \
+find ../$REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" \) \
   -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
     if grep -q "Copyright Ascensio System SIA" "$file"; then
         perl -i -0777 -pe 's|/\*.*?Copyright Ascensio System SIA.*?\*/||gs' "$file"
