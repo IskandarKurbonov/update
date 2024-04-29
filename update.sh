909f7aec7ee9b0917983ffc5032f4661ceb0de3e
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-04-29 22:29:17 +0500

    Add php files

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-find ../$REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" \) \
+find ../$REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" \) \
   -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
-    if grep -q "Ascensio System SIA" "$file"; then
-        perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
+    if grep -q "Copyright Ascensio System SIA" "$file"; then
+        perl -i -0777 -pe 's|/\*.*?Copyright Ascensio System SIA.*?\*/||gs' "$file"
         echo "$file" >> success.log
-        reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="template" "$file"
+        reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
     fi
 done
