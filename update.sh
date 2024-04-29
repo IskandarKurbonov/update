489c8d92deec07f5237e15c60c4800b641df8e40
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-04-29 16:12:53 +0500

    Some refactoring

--- update.sh
+++ update.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-for REPO in ${REPO[@]}; do
-  if [[ ! -f ../${REPO} ]]; then
-    echo "Directory ${REPO} doesn't exist!"
-    exit 0
-  fi
-  echo "Start updating..."
-done
-
 find /home/runner/work/update/$REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" \) \
   -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
     if grep -q "Ascensio System SIA" "$file"; then
