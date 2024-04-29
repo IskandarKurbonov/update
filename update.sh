207ff266ad8f3f53618c2669203735da9069c386
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-04-29 14:02:39 +0500

    Add default files

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+REPO="document-server-integration/"
+
+find $REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" \) \
+  -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
+    if grep -q "Ascensio System SIA" "$file"; then
+        perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
+        echo "$file" >> success.log
+        reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="template" "$file"
+    fi
+done
