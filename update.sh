58053e1577d96d47ffd43000c871957b7d667726
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-04-30 12:38:29 +0500

    Add new regexp

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ find ../$REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -nam
   -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
     if grep -q "Copyright Ascensio System SIA" "$file"; then
         perl -i -0777 -pe 's|/\*.*?Copyright Ascensio System SIA.*?\*/||gs' "$file"
-        echo "$file" >> success.log
+        perl -i -0777 -pe 's|""".*?Copyright Ascensio System SIA.*?"""||gs' "$file"
         reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
     fi
 done
