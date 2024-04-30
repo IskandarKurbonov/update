8cad89ef096011276e34c13510e21b6bd51a5611
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-04-30 15:38:25 +0500

    add variables

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-find ../$REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" -o -name "*.html" \) \
-  -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
+FILE_FORMATS=".js|.java|.css|.php|.rb|.py|.html"
+
+IGNORE_PATHS=".git|.github"
+
+find ../$REPO -type f \( -name "*$FILE_FORMATS" \) \
+  -not \( -path "$REPO/$IGNORE_PATHS*" \) | while read -r file; do
     if grep -q "Copyright Ascensio System SIA" "$file"; then
         perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
         perl -i -0777 -pe 's|""".*?Ascensio System SIA.*?"""||gs' "$file"
