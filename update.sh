ef4361e93f968d7b6c1bb9817baf34fabcce65c7
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-04-30 13:52:53 +0500

    some refactoring

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find ../$REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" -o -name "*.html"\) \
+find ../$REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" -o -name "*.html" \) \
   -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
     if grep -q "Copyright Ascensio System SIA" "$file"; then
         perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
