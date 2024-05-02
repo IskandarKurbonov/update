c741952ebcacd899fd1a62f25e838644e0e60f9d
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-05-02 16:32:30 +0500

    Some refactoring

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-find ../$REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" -o -name "*.html" \) \
+find ../$REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" -o -name "*.html" -o -name "*.json" -o -name "*.conf" -o -name "*.bat"-o -name "*.sh"\) \
   -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
     if grep -q "Ascensio System SIA" "$file"; then
         perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
         perl -i -0777 -pe 's|""".*?Ascensio System SIA.*?"""||gs' "$file"
         perl -i -0777 -pe 's|<!--.*?Ascensio System SIA.*?-->||gs' "$file"
+        reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
+    else
         reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
     fi
 done
