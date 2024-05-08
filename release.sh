2bf1aea5bfe8371e4c3e2d0770f246fdd1962e51
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-05-08 13:00:33 +0500

    Some refactoring

--- release.sh
+++ release.sh
@@ -1,46 +1,51 @@
 #!/usr/bin/env bash
 
 set -e
+GITHUB_TOKEN=$1
+GITHUB_USER=$3
+VERSION=$2
+REPO_TAG=v$VERSION
+RELEASE_TAG=v${VERSION%.*}
 
 OUTPUT_DIR=./update
+OUTPUT_FILE_NAME=$REPO_TAG.zip
 
 ONLYOFFICE_REPOS=()
 ONLYOFFICE_REPOS+=('document-server-integration')
 ONLYOFFICE_REPOS+=('sdkjs')
 ONLYOFFICE_REPOS+=('web-apps')
 
-for REPO in ${ONLYOFFICE_REPOS[@]}
+rm -rf ${OUTPUT_DIR}
+
+for REPO in ${ONLYOFFICE_REPOS[*]}
 do
   curl -H 'Authorization: token '$GITHUB_TOKEN \
   -H 'Accept: application/vnd.github.v3.raw' \
   -O \
   -s \
-  -L https://github.com/$GITHUB_USER/$REPO.git
+  -L https://github.com/$GITHUB_USER/$REPO/archive/$REPO_TAG.zip
 
+  FILE_NAME=$REPO.zip
+  mv $REPO_TAG.zip $FILE_NAME
+  unzip -q $FILE_NAME -d $OUTPUT_DIR
+  mv $OUTPUT_DIR/$REPO-$VERSION $OUTPUT_DIR/$REPO
+  rm $FILE_NAME
 done
 
-ls -lha
-pwd
-tree
-unzip main.zip
-ls -lha
-ls -lha main
+rm -f ${OUTPUT_FILE_NAME}
+zip -r -q ${OUTPUT_FILE_NAME} ${OUTPUT_DIR}
 
-
-  # Run update.sh script
-  # chmod +x ./update.sh
-  # ./update.sh $REPO
-
-# URL=https://github.com/IskandarKurbonov/update/releases/download/${RELEASE_TAG}/${OUTPUT_FILE_NAME}
+URL=https://github.com/$GITHUB_USER/$OUTPUT_DIR/releases/download/${RELEASE_TAG}/${OUTPUT_FILE_NAME}
      
-# cat << EOF >> release_hash.txt
-# "Size: $(wc -c ${OUTPUT_FILE_NAME} | awk '{print $1}')"
-# "md5sum: $(md5sum -b ${OUTPUT_FILE_NAME} | awk '{print $1}')"
-# "sha256sum: $(sha256sum -b ${OUTPUT_FILE_NAME} | awk '{print $1}')"
-# EOF
+cat << EOF >> release_hash.txt
+"Url: ${URL}"
+"Size: $(wc -c ${OUTPUT_FILE_NAME} | awk '{print $1}')"
+"md5sum: $(md5sum -b ${OUTPUT_FILE_NAME} | awk '{print $1}')"
+"sha256sum: $(sha256sum -b ${OUTPUT_FILE_NAME} | awk '{print $1}')"
+EOF
 
-# git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/IskandarKurbonov/update.git ${OUTPUT_DIR}/update
-# cd ${OUTPUT_DIR}/update
-# git tag ${RELEASE_TAG}
-# git push origin ${RELEASE_TAG}
-# gh release create ${RELEASE_TAG} ../../${OUTPUT_FILE_NAME} ../../release_hash.txt
+git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/$GITHUB_USER/update.git ${OUTPUT_DIR}/update
+cd ${OUTPUT_DIR}/update
+git tag ${RELEASE_TAG}
+git push origin ${RELEASE_TAG}
+gh release create ${RELEASE_TAG} ../../${OUTPUT_FILE_NAME} ../../release_hash.txt
