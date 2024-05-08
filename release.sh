02c3ec39994b5c5f3988eeb374efab15b068d4bd
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-05-08 17:20:10 +0500

    Some refactoring

--- release.sh
+++ release.sh
@@ -12,7 +12,17 @@ do
     ls -lha
     git clone https://github.com/$GITHUB_USER/$REPO.git
     ls -lha
-    ./update.sh
+    find $REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" -o -name "*.html" -o -name "*.bat" -o -name "*.sh" \) \
+        -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
+      if grep -q "Ascensio System SIA" "$file"; then
+          perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
+          perl -i -0777 -pe 's|""".*?Ascensio System SIA.*?"""||gs' "$file"
+          perl -i -0777 -pe 's|<!--.*?Ascensio System SIA.*?-->||gs' "$file"
+          reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
+      else
+          reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
+      fi
+    done
 done
 
 
