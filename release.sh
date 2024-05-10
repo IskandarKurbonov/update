46d5acc4e5f5b4de29c992e9a379d18684b85f39
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-05-10 16:06:16 +0500

    Some refactoring

--- release.sh
+++ release.sh
@@ -12,18 +12,18 @@ do
     ls -lha
     git clone https://github.com/$GITHUB_USER/$REPO.git
     ls -lha
-    # find $REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" -o -name "*.html" -o -name "*.bat" -o -name "*.sh" \) \
-    #     -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
-    #   if grep -q "Ascensio System SIA" "$file"; then
-    #       perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
-    #       perl -i -0777 -pe 's|""".*?Ascensio System SIA.*?"""||gs' "$file"
-    #       perl -i -0777 -pe 's|<!--.*?Ascensio System SIA.*?-->||gs' "$file"
-    #       reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
-    #   else
-    #       reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
-    #   fi
-    # done
-
+    find $REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" -o -name "*.html" -o -name "*.bat" -o -name "*.sh" \) \
+        -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
+      if grep -q "Ascensio System SIA" "$file"; then
+          perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
+          perl -i -0777 -pe 's|""".*?Ascensio System SIA.*?"""||gs' "$file"
+          perl -i -0777 -pe 's|<!--.*?Ascensio System SIA.*?-->||gs' "$file"
+          reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
+      else
+          reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
+      fi
+    done
+    
     zip -r master.zip $REPO
     rm -rf $REPO
 done
