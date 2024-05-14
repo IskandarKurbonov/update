fe250fb9e677766e297a5f1ab2860b915212c5e2
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-05-14 17:13:18 +0500

    Some refactoring

--- release.sh
+++ release.sh
@@ -31,10 +31,10 @@ do
 
 done
 
-	cat release_hash.txt <<-EOF
-		"Size: $(wc -c master.zip | awk '{print $1}')"
-		"md5sum: $(md5sum -b master.zip | awk '{print $1}')"
-		"sha256sum: $(sha256sum -b master.zip | awk '{print $1}')"
+    cat release_hash.txt <<-EOF
+        "Size: $(wc -c master.zip | awk '{print $1}')"
+        "md5sum: $(md5sum -b master.zip | awk '{print $1}')"
+        "sha256sum: $(sha256sum -b master.zip | awk '{print $1}')"
 EOF
 
 git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/update.git ./update/update
