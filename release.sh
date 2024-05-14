2cfa2f2b02dab554a7c38c904fcabd0988e02a3f
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-05-14 13:34:39 +0500

    Some refactoring

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 MAIN_DIR=./update
+REPO_TAG=v$VERSION
+RELEASE_TAG=v${VERSION%.*}
 
 ONLYOFFICE_REPOS=()
 ONLYOFFICE_REPOS+=('document-server-integration')
@@ -28,11 +30,16 @@ do
     rm -rf $REPO
 
     cat << EOF >> release_hash.txt
-        "Url: ${URL}"
         "Size: $(wc -c ${OUTPUT_FILE_NAME} | awk '{print $1}')"
         "md5sum: $(md5sum -b ${OUTPUT_FILE_NAME} | awk '{print $1}')"
         "sha256sum: $(sha256sum -b ${OUTPUT_FILE_NAME} | awk '{print $1}')"
     EOF
 done
+
+git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/update.git ./update/update
+cd ./update/update
+git tag ${RELEASE_TAG}
+git push origin ${RELEASE_TAG}
+gh release create ${RELEASE_TAG} ../../master.zip ../../release_hash.txt
 
 
