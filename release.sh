1ed4f3d32ad2f06e2e8f4ffe50559d6d30b078b9
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-05-14 17:11:37 +0500

    Some refactoring

--- release.sh
+++ release.sh
@@ -31,7 +31,7 @@ do
 
 done
 
-cat <<EOF>> release_hash.txt
+cat release_hash.txt <<-EOF
     "Size: $(wc -c master.zip | awk '{print $1}')"
     "md5sum: $(md5sum -b master.zip | awk '{print $1}')"
     "sha256sum: $(sha256sum -b master.zip | awk '{print $1}')"
