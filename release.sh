669f443f3522963735488d405e1a161948fd301c
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-05-14 16:14:19 +0500

    Some refactoring

--- release.sh
+++ release.sh
@@ -29,13 +29,14 @@ do
     zip -r master.zip $REPO
     rm -rf $REPO
 
-done
-
 cat << DAO >> release_hash.txt
 "Size: $(wc -c master.zip | awk '{print $1}')"
 "md5sum: $(md5sum -b master.zip | awk '{print $1}')"
 "sha256sum: $(sha256sum -b master.zip | awk '{print $1}')"
 DAO
+
+done
+
 
 git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/update.git ./update/update
 cd ./update/update
