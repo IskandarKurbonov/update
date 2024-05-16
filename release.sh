16f4a486be9fb16320e79a063ffd93e2543dea68
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-05-16 14:57:34 +0500

    Some refactoring

--- release.sh
+++ release.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-MAIN_DIR=./update
-REPO_TAG=v$VERSION
 RELEASE_TAG=v${VERSION%.*}
-OUTPUT_FILE_NAME=$REPO_TAG.zip
+OUTPUT_FILE_NAME=archive.zip
 
 ONLYOFFICE_REPOS=()
 ONLYOFFICE_REPOS+=('document-server-integration')
@@ -14,8 +12,9 @@ do
     git clone https://github.com/$GITHUB_USER/$REPO.git
 
     find $REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" -o -name "*.html" -o -name "*.bat" -o -name "*.sh" \) \
-        -not \( -path "$REPO/.git/*" -o -path "$REPO/.github/*" \) | while read -r file; do
+         | while read -r file; do
       if grep -q "Ascensio System SIA" "$file"; then
+          rm -rf .git/ .github/
           perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
           perl -i -0777 -pe 's|""".*?Ascensio System SIA.*?"""||gs' "$file"
           perl -i -0777 -pe 's|<!--.*?Ascensio System SIA.*?-->||gs' "$file"
