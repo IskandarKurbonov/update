b89371f1344582d69308a48635f624992b0a6b4e
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-05-17 10:47:10 +0500

    Some refactoring

--- release.sh
+++ release.sh
@@ -6,28 +6,26 @@ OUTPUT_FILE_NAME=archive.zip
 ONLYOFFICE_REPOS=()
 ONLYOFFICE_REPOS+=('document-server-integration')
 
-for REPO in ${ONLYOFFICE_REPOS[*]}
-do
-
+for REPO in ${ONLYOFFICE_REPOS[*]}; do
     git clone --depth 1 https://github.com/$GITHUB_USER/$REPO.git
 
-    find $REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" -o -name "*.html" -o -name "*.bat" -o -name "*.sh" -o -name "*.scss" -o -name "*.csh" -o -name "*.cpp"\) \
-         | while read -r file; do
-      if grep -q "Ascensio System SIA" "$file"; then
-          perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
-          perl -i -0777 -pe 's|""".*?Ascensio System SIA.*?"""||gs' "$file"
-          perl -i -0777 -pe 's|<!--.*?Ascensio System SIA.*?-->||gs' "$file"
-          reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
-      else
-          reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
-      fi
+    find $REPO -type f \( \
+        -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o \
+        -name "*.rb" -o -name "*.py" -o -name "*.html" -o -name "*.bat" -o \
+        -name "*.sh" -o -name "*.scss" -o -name "*.csh" -o -name "*.cpp" \) | \
+    while read -r file; do
+        if grep -q "Ascensio System SIA" "$file"; then
+            perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
+            perl -i -0777 -pe 's|""".*?Ascensio System SIA.*?"""||gs' "$file"
+            perl -i -0777 -pe 's|<!--.*?Ascensio System SIA.*?-->||gs' "$file"
+        fi
+        reuse annotate --year 2024 --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
     done
-    
+
     rm -rf $REPO/.git*
     
     zip -r -q ${OUTPUT_FILE_NAME} $REPO
     rm -rf $REPO
-
 done
 
 cat << EOF >> release_hash.txt
