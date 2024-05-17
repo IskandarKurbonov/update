1e3de5f880e69fdd758c1245d948f9c09ccfe1d1
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-05-17 10:42:22 +0500

    Some refactoring

--- release.sh
+++ release.sh
@@ -11,7 +11,7 @@ do
 
     git clone --depth 1 https://github.com/$GITHUB_USER/$REPO.git
 
-    find $REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" -o -name "*.html" -o -name "*.bat" -o -name "*.sh" \) \
+    find $REPO -type f \( -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o -name "*.rb" -o -name "*.py" -o -name "*.html" -o -name "*.bat" -o -name "*.sh" -o -name "*.scss" -o -name "*.csh" -o -name "*.cpp"\) \
          | while read -r file; do
       if grep -q "Ascensio System SIA" "$file"; then
           perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
