1042eec1ae526ba3d0fc80b6d13b5ece3fd110e7
IskandarKurbonov <iskandar.kurbonov@onlyoffice.com>
2024-05-22 16:43:33 +0500

    Some refactoring

--- release.sh
+++ release.sh
@@ -18,16 +18,17 @@ for REPO in ${ONLYOFFICE_REPOS[*]}; do
         -name "*.js" -o -name "*.java" -o -name "*.css" -o -name "*.php" -o \
         -name "*.rb" -o -name "*.py" -o -name "*.html" -o -name "*.bat" -o \
         -name "*.sh" -o -name "*.scss" -o -name "*.cs" -o -name "*.cpp" -o  \
-        -name "*.jsp" -o -name "*.aspx" \) | \
+        -name "*.jsp" -o -name "*.aspx" -o -name "*.ejs" -o -name "*.c" -o \
+        -name "*.h" -o -name "*.cxx" \) | \
     while read -r file; do
         if grep -q "Ascensio System SIA" "$file"; then
-            perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file" 
-            perl -i -0777 -pe 's|\/\/.*?Ascensio System SIA.*?$.*?\/\/.*?$||gs' "$file"
+            perl -i -0777 -pe 's|/\*.*?Ascensio System SIA.*?\*/||gs' "$file"
             perl -i -0777 -pe 's|""".*?Ascensio System SIA.*?"""||gs' "$file"  
             perl -i -0777 -pe 's|<!--.*?Ascensio System SIA.*?-->||gs' "$file"  
             perl -i -0777 -pe 's|^#.*?Ascensio System SIA.*?$.*?^#.*?$||gs' "$file"  
             perl -i -0777 -pe 's|=begin.*?Ascensio System SIA.*?=end||gs' "$file"  
             perl -i -0777 -pe 's|//.*?Ascensio System SIA.*?$.*?//.*?$||gs' "$file"  
+            perl -i -0777 -pe 's|//.*?Ascensio System SIA.*?$.*?//.*?$||gs' "$file"
             perl -i -0777 -pe 's|REM.*?Ascensio System SIA.*?$.*?REM.*?$||gs' "$file"  
         fi
         reuse annotate --year $YEAR --license Ascensio-System --copyright="Ascensio System SIA" --template="license" "$file"
